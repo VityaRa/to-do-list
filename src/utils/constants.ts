b42e8be54a71c52a2b1a15b7920680929d4fa805
VityaRa <blackops2in@mail.ru>
2021-09-03 03:04:00 +0300

    feat: add production ref

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = process.env.NODE_ENV === 'development' ?  'http://localhost:3001' : 'https://enigmatic-island-13804.herokuapp.com'
