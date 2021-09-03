69593092d8a2c18ab3871ce32585c1c9a875357b
VityaRa <blackops2in@mail.ru>
2021-09-03 05:46:36 +0300

    feat: add icons, service workers for pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React, { lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom';
+//@ts-nocheck
 
-import './index.scss';
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./index.scss";
 
-import { store } from './store';
-import { Provider } from 'react-redux';
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-const App = lazy(() => import('./App'));
+const App = lazy(() => import("./App"));
 
 ReactDOM.render(
   <Provider store={store}>
-     <Suspense fallback={<h2 style={{color: '#000'}}>WAIT</h2>}>
+    <Suspense fallback={<h2 style={{ color: "#000" }}>WAIT</h2>}>
       <App />
-     </Suspense>
+    </Suspense>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
