1ff4bc08dca764bdc2905cbd2b8591267530f1d0
VityaRa <blackops2in@mail.ru>
2021-09-03 08:11:53 +0300

    feat: add jwt auth

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectId;
 const cors = require('cors')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require("dotenv").config();
+const auth = require("./middleware/auth");
 
 const app = express();
 const jsonParser = express.json();
@@ -16,7 +21,7 @@ const port = process.env.PORT || 3001;
 (async () => {
     try {
         await mongoClient.connect();
-        app.locals.collection = mongoClient.db("listdb").collection("items");
+        app.locals.collection = mongoClient.db("listdb");
         app.listen(port, () => {
             console.log("Сервер ожидает подключения...");
         })
@@ -28,7 +33,7 @@ const port = process.env.PORT || 3001;
 
 
 app.get("/api/items", async (req, res) => {
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
     try {
         const items = await collection.find({}).toArray();
         res.send(items);
@@ -40,7 +45,7 @@ app.get("/api/items", async (req, res) => {
 app.get("/api/users/:id", async (req, res) => {
 
     const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
     try {
         const user = await collection.findOne({ _id: id });
         res.send(user);
@@ -57,7 +62,7 @@ app.post("/api/item", jsonParser, async (req, res) => {
 
     const item = { description, isDone: false };
 
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
 
     try {
         await collection.insertOne(item);
@@ -68,7 +73,7 @@ app.post("/api/item", jsonParser, async (req, res) => {
 
 app.delete("/api/item/:id", async (req, res) => {
     const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
     try {
         const result = await collection.findOneAndDelete({ _id: id });
         const item = result.value;
@@ -85,7 +90,7 @@ app.put("/api/item/desc", jsonParser, async (req, res) => {
     const { description } = req.body
     if (!description) res.sendStatus(400);
 
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
     try {
         const result = await collection.findOneAndUpdate({ _id: id }, { $set: { description } },
             { returnDocument: "after" });
@@ -101,7 +106,7 @@ app.put("/api/item/status", jsonParser, async (req, res) => {
     const id = new objectId(req.body.id);
 
     const { isDone } = req.body
-    const collection = req.app.locals.collection;
+    const collection = req.app.locals.collection.collection("items");
     try {
         const result = await collection.findOneAndUpdate({ _id: id }, { $set: { isDone } },
             { returnDocument: "after" });
@@ -111,7 +116,79 @@ app.put("/api/item/status", jsonParser, async (req, res) => {
     catch (err) { return console.log(err); }
 });
 
-// прослушиваем прерывание работы программы (ctrl-c)
+app.post("/api/register", jsonParser, async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        const collection = req.app.locals.collection.collection("users");
+
+        const oldUser = await collection.findOne({ email });
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await collection.insertOne({
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+
+        user.token = token;
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post("/api/login", async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        const collection = req.app.locals.collection.collection("users");
+
+        const user = await collection.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
+
+            user.token = token;
+
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+
 process.on("SIGINT", async () => {
 
     await mongoClient.close();
