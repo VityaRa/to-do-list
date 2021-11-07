f83cf18bcdd14380a7d058cdd07d50f3ee5fbb9f
VityaRa <blackops2in@mail.ru>
2021-11-07 17:20:15 +0300

    feat: add token checking

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const port = process.env.PORT || 3001;
         app.listen(port, () => {
             console.log("Сервер ожидает подключения...");
         })
-    
-        
+
+
     } catch (err) {
         return console.log(err);
     }
@@ -61,7 +61,7 @@ app.post("/api/register", jsonParser, async (req, res) => {
         })
 
         const token = jwt.sign(
-            { user_id: user._id.toString(), email },
+            { user_id: user._id, email },
             process.env.TOKEN_KEY,
             {
                 expiresIn: "2h",
@@ -93,7 +93,7 @@ app.post("/api/login", jsonParser, async (req, res) => {
         if (user && (await bcrypt.compare(password, user.password))) {
 
             const token = jwt.sign(
-                { user_id: user._id.toString(), email },
+                { user_id: user._id, email },
                 process.env.TOKEN_KEY,
                 {
                     expiresIn: "2h",
@@ -111,12 +111,23 @@ app.post("/api/login", jsonParser, async (req, res) => {
     }
 });
 
+app.get("/api/user", jsonParser, auth, async (req, res) => {
+
+    const collection = req.app.locals.collection.collection("users");
+
+    try {
+        const user = await collection.findOne({ email: req.user });
+        res.send(user);
+    }
+    catch (err) { return console.log(err); }
+});
+
 //get all lists
 app.get("/api/list", jsonParser, auth, async (req, res) => {
 
     const collection = req.app.locals.collection.collection("lists");
     try {
-        const items = await collection.find({userId: req.userId}, { projection: { userId: 0 } }).toArray();
+        const items = await collection.find({ email: req.user }, { projection: { userId: 0 } }).toArray();
         res.send(items);
     }
     catch (err) { return console.log(err); }
@@ -128,7 +139,7 @@ app.get("/api/list/:id", jsonParser, auth, async (req, res) => {
     const id = new objectId(req.params.id);
 
     try {
-        const list = await collection.findOne({ _id: id, userId: req.userId }, { projection: { userId: 0 } });
+        const list = await collection.findOne({ _id: id, email: req.user }, { projection: { userId: 0 } });
         res.send(list);
     }
     catch (err) { return console.log(err); }
@@ -140,13 +151,12 @@ app.post("/api/list", jsonParser, auth, async (req, res) => {
 
     const { title } = req.body
     if (!title) res.sendStatus(400);
-    console.log(req.userId);
-    const newList = { title, items: [], userId: req.userId }
+    const newList = { title, items: [], email: req.user }
     const collection = req.app.locals.collection.collection("lists");
 
     try {
         await collection.insertOne(newList);
-        delete newList.userId;
+        delete newList.email;
         res.send(newList);
     }
 
@@ -158,7 +168,7 @@ app.delete("/api/list/:id", jsonParser, auth, async (req, res) => {
     const id = new objectId(req.params.id);
     const collection = req.app.locals.collection.collection("lists");
     try {
-        const result = await collection.findOneAndDelete({ _id: id, userId: req.userId});
+        const result = await collection.findOneAndDelete({ _id: id, email: req.user });
         const item = result.value;
         res.send(item);
     }
@@ -192,7 +202,7 @@ app.put("/api/list/:id/:itemId", jsonParser, auth, async (req, res) => {
 
     const collection = req.app.locals.collection.collection("lists");
     try {
-        const result = await collection.findOneAndUpdate({ _id: id, userId: req.userId }, { $pull: { items: { _id: itemId } } }, { returnDocument: "after" });
+        const result = await collection.findOneAndUpdate({ _id: id, email: req.user }, { $pull: { items: { _id: itemId } } }, { returnDocument: "after" });
         const item = result.value;
         res.send({ success: true });
     }
@@ -251,7 +261,7 @@ app.put("/api/list/title", jsonParser, auth, async (req, res) => {
     try {
         const result = await collection.findOneAndUpdate({ _id }, { $set: { title } },
             { returnDocument: "after" });
-        
+
         res.send({ title: result.value.title, _id: result.value._id, items: [] });
     }
     catch (err) { return console.log(err); }
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 const config = process.env;
-const objectId = require("mongodb").ObjectId;
 
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
-
-  const userId = req.body.userId || req.query.userId;
-  const userEmail = req.body.email || req.query.email;
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
@@ -20,10 +16,9 @@ const verifyToken = (req, res, next) => {
     return res.status(401).send("Invalid Token");
   }
 
-  if (userEmail !== req.user) {
-    return res.status(409).send("Wrong email")
-  }
-  req.userId = userId
+  // if (userEmail !== req.user) {
+  //   return res.status(409).send("Wrong email")
+  // }
 
   return next();
 };
